af707533b159315ab65dca3ac538c76a265ba510
Rakshitmv <rakshit8976646711@gmail.com>
2022-01-11 21:28:24 +0530

    harmonic code update

---
+++ harmonic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+
+read -p "Enter the num : " num
+harmonic=0
+
+for((i=1;i<=$num;i++))
+do 
+	ans=`awk "BEGIN {print (1/$i)}"`
+	harmonic=`awk "BEGIN {print ($harmonic+$ans)}"`
+	#echo $ans
+done
+echo "The harmonic number for range (1 to $num) is: " $harmonic
